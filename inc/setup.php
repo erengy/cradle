a4c332e5fa8357f244dc7f1de30b9d04688dc1ca
Eren Okka <erenokka@gmail.com>
2020-11-03 02:14:06 +0300

    Load translated strings

--- inc/setup.php
+++ inc/setup.php
@@ -4,17 +4,25 @@ namespace Cradle;
 
 add_action('after_setup_theme', function () {
 	/**
+	 * @link https://developer.wordpress.org/reference/functions/load_theme_textdomain/
+	 */
+	load_theme_textdomain('cradle', get_template_directory() . '/languages');
+
+	/**
 	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/
 	 */
-	add_theme_support('html5', array(
-			'comment-list',
-			'comment-form',
-			'search-form',
-			'gallery',
-			'caption',
-			'style',
-			'script',
-		));
+	add_theme_support(
+			'html5',
+			array(
+					'comment-list',
+					'comment-form',
+					'search-form',
+					'gallery',
+					'caption',
+					'style',
+					'script',
+				)
+		);
 	add_theme_support('post-thumbnails');
 	add_theme_support('title-tag');
 
