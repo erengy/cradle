9ff635854696abf5280b38f5c05b5bb439f8d2d6
Eren Okka <erenokka@gmail.com>
2020-11-14 01:57:03 +0300

    Move post thumbnail function

---
+++ inc/template-tags/post-thumbnail.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link https://developer.wordpress.org/themes/basics/template-tags/
+ */
+
+namespace Cradle;
+
+function get_post_thumbnail_url($size = 'thumbnail', $post = null) {
+	$thumbnail_id = get_post_thumbnail_id($post);
+	if ($thumbnail_id) {
+		$image = wp_get_attachment_image_src($thumbnail_id, $size, false);
+		if ($image) {
+			return $image[0];
+		}
+	}
+	return get_template_directory_uri() . '/assets/img/placeholder.png';
+}
