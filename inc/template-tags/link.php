62f8885cd30deab607fe047a548ba4a8a1f85cbb
Eren Okka <erenokka@gmail.com>
2020-11-14 01:36:35 +0300

    Add function to get post thumbnail URL

--- inc/template-tags/link.php
+++ inc/template-tags/link.php
@@ -8,3 +8,14 @@ namespace Cradle;
 function get_asset_url($file) {
 	return get_template_directory_uri() . '/assets' . $file;
 }
+
+function get_post_thumbnail_url($size = 'thumbnail', $post = null) {
+	$thumbnail_id = get_post_thumbnail_id($post);
+	if ($thumbnail_id) {
+		$image = wp_get_attachment_image_src($thumbnail_id, $size, false);
+		if ($image) {
+			return $image[0];
+		}
+	}
+	return get_template_directory_uri() . '/assets/img/placeholder.png';
+}
