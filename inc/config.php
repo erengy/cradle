e6e450e0ea4b46cc8131943f8d5d441b5372b0f3
Eren Okka <erenokka@gmail.com>
2021-03-02 08:10:38 +0300

    Add theme options

--- inc/config.php
+++ inc/config.php
@@ -7,3 +7,18 @@ const BASE_TEMPLATE = '_base.php';
 const COMMENTS_TEMPLATE = 'partials/comments.php';
 
 const EXCERPT_LENGTH = 30;
+
+const THEME_OPTIONS = array(
+		'contact' => array(
+				'address' => '221B Baker Street',
+				'email'   => 'info@example.com',
+				'phone'   => '+90 555 123 45 67',
+			),
+		'social' => array(
+				'Facebook'  => '#',
+				'Instagram' => '#',
+				'LinkedIn'  => '#',
+				'Twitter'   => '#',
+				'YouTube'   => '#',
+			),
+	);
