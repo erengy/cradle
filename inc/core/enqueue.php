d376ae2a1d0e680ab4f27ca13a260371a2ee98e2
Eren Okka <erenokka@gmail.com>
2021-04-15 14:46:53 +0300

    Move core files

---
+++ inc/core/enqueue.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Cradle;
+
+/**
+ * Changed default $ver to `null`.
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_enqueue_style/
+ */
+function enqueue_style($handle, $src = '', $deps = array(), $ver = null, $media = 'all') {
+	wp_enqueue_style($handle, $src, $deps, $ver, $media);
+}
+
+/**
+ * Changed default $ver to `null`.
+ * Changed default $in_footer to `true`.
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+ */
+function enqueue_script($handle, $src = '', $deps = array(), $ver = null, $in_footer = true) {
+	wp_enqueue_script($handle, $src, $deps, $ver, $in_footer);
+}
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -2,25 +2,6 @@
 
 namespace Cradle;
 
-/**
- * Changed default $ver to `null`.
- *
- * @link https://developer.wordpress.org/reference/functions/wp_enqueue_style/
- */
-function enqueue_style($handle, $src = '', $deps = array(), $ver = null, $media = 'all') {
-	wp_enqueue_style($handle, $src, $deps, $ver, $media);
-}
-
-/**
- * Changed default $ver to `null`.
- * Changed default $in_footer to `true`.
- *
- * @link https://developer.wordpress.org/reference/functions/wp_enqueue_script/
- */
-function enqueue_script($handle, $src = '', $deps = array(), $ver = null, $in_footer = true) {
-	wp_enqueue_script($handle, $src, $deps, $ver, $in_footer);
-}
-
 add_action('wp_enqueue_scripts', function () {
 	if (class_exists('Classic_Editor')) {
 		wp_dequeue_style('wp-block-library');
