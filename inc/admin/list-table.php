cbc87aa6dbc23f19c9a908a3056391d6076ecd86
Eren Okka <erenokka@gmail.com>
2023-04-28 17:37:53 +0300

    Add menu order column for pages

---
+++ inc/admin/list-table.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Cradle;
+
+/**
+ * Add menu order column for pages
+ *
+ * @link https://developer.wordpress.org/reference/hooks/manage_post_type_posts_columns/
+ * @link https://developer.wordpress.org/reference/hooks/manage_post-post_type_posts_custom_column/
+ */
+add_filter('manage_page_posts_columns', function ($columns) {
+	$columns['menu_order'] = __('Order', 'cradle');
+	return $columns;
+}, 10);
+
+add_action('manage_page_posts_custom_column', function ($column_key, $post_id) {
+	switch ($column_key) {
+		case 'menu_order':
+			if ($_post = get_post($post_id)) {
+				echo $_post->menu_order;
+			}
+			break;
+	}
+}, 10, 2);
