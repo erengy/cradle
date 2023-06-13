40864ebbe567b1d5459f1b3506870b8e4a31d5bb
Eren Okka <erenokka@gmail.com>
2023-06-13 17:46:16 +0300

    Update Bootstrap to 5.3.0

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,7 +11,7 @@ add_action('wp_enqueue_scripts', function () {
 	}
 	enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,500,700&display=swap&subset=latin-ext');
 	enqueue_style('aos', 'https://cdn.jsdelivr.net/npm/aos@3.0.0-beta.6/dist/aos.css');
-	enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css');
+	enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css');
 	enqueue_style('photoswipe', 'https://cdn.jsdelivr.net/npm/photoswipe@4.1.3/dist/photoswipe.css');
 	enqueue_style('photoswipe-skin', 'https://cdn.jsdelivr.net/npm/photoswipe@4.1.3/dist/default-skin/default-skin.css');
 	enqueue_style('swiper', 'https://cdn.jsdelivr.net/npm/swiper@8.4.6/swiper-bundle.min.css');
@@ -21,8 +21,8 @@ add_action('wp_enqueue_scripts', function () {
 		enqueue_script('comment-reply');
 	}
 	enqueue_script('fontawesome', 'https://use.fontawesome.com/releases/v6.2.1/js/all.js', [], null, false);
-	enqueue_script('popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js');
-	enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js', ['popper']);
+	enqueue_script('popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js');
+	enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js', ['popper']);
 	enqueue_script('aos', 'https://cdn.jsdelivr.net/npm/aos@3.0.0-beta.6/dist/aos.js');
 	enqueue_script('photoswipe', 'https://cdn.jsdelivr.net/npm/photoswipe@4.1.3/dist/photoswipe.min.js');
 	enqueue_script('photoswipe-ui', 'https://cdn.jsdelivr.net/npm/photoswipe@4.1.3/dist/photoswipe-ui-default.min.js');
