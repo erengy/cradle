667e77c6a847471fad1602d472f9476bae30d83b
Eren Okka <erenokka@gmail.com>
2023-06-13 17:49:25 +0300

    Remove tag rewrite rules filter
    Moved to mu-plugins.

--- inc/rewrite.php
+++ inc/rewrite.php
@@ -6,7 +6,6 @@ add_filter('author_rewrite_rules', '__return_empty_array');
 add_filter('category_rewrite_rules', '__return_empty_array');
 add_filter('date_rewrite_rules', '__return_empty_array');
 add_filter('post_format_rewrite_rules', '__return_empty_array');
-add_filter('post_tag_rewrite_rules', '__return_empty_array');
 
 add_filter('rewrite_rules_array', function ($rules) {
 	foreach ($rules as $rule => $rewrite) {
